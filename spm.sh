2fa43357eeba1d8b21566806ba6a23577be29b40
Jchase2 <JC835072@WCUPA.EDU>
2015-09-21 17:05:19 -0400

    Added a lot of comments for readability. Also, added a backup directory. This is for future additions to the backup function. Also added additional info to improve usability.

--- spm.sh
+++ spm.sh
@@ -13,24 +13,30 @@
 
 #Read in filename of GPG file.
 
-GLOBV=""
-SGLOBV=""
-ENDVAR="END"
-USEKEY=""
+GLOBV="" #Stores initial user input.
+SGLOBV="" #Choice of header or string for search function.
+ENDVAR="END" #End of block delimiter.
+USEKEY="" #Beginning of block delmiter. E.g header name.
 
 gpg_function(){
   check_mem
-  echo $'\n'
-  echo -n "GPG Encrypted Password File Name: "
+  echo "Use tab to search / for completion"
+  echo -n "GPG Encrypted Password File Path: "
   read -e pwfile
   PVAR=$(gpg --decrypt $pwfile)
   echo $'\n'
   welcome_function
 }
 
+# Creates a backup directory and creates
+# backups upon pw file change.
+
 backup_function(){
-  cp $pwfile ${pwfile}.bak
-  if [ $? -gt 0 ]; then
+  if [ ! -d pwbackup ] ; then
+    mkdir pwbackup
+  fi
+  cp $pwfile pwbackup/${pwfile}.bak
+  if [ $? -gt 0 ]; then # If cp exits with non-zero return.
     echo "Backup Failed."
   else
     echo "Successful Backup."
@@ -93,6 +99,7 @@ input_function(){
    fi
 }
 
+#TODO: Return not found if it doesn't exist.
 search_function() {
    tmpresult="$SGLOBV"
    if [ $tmpresult -eq '1' ] ; then
@@ -127,13 +134,13 @@ check_mem(){
      echo -n "cause data to overflow into swap on the drive. Would you like to continue? y/n: "
        read YN
      if [[ $YN =~ [yY](es)* ]]  ; then
-        echo "Continuing anyway." 
+        echo "Continuing anyway."
      else
        exit
      fi
    else
       echo "Memory ok."
-   fi 
+   fi
 }
 
 new_pw(){
